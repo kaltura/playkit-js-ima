42efc74de69d90595df316066df2c6f322d1f7b2
Dan Ziv <dan.ziv@kaltura.com>
2017-06-12 23:29:18 +0300

    create decorator file

--- src/ima.js
+++ src/ima.js
@@ -1,4 +1,5 @@
 import {registerPlugin, BasePlugin, PlayerDecoratorBase} from 'playkit-js'
+import ImaDecorator from './decorator'
 
 const pluginName = "ima";
 
@@ -14,27 +15,20 @@ class ImaPlugin extends BasePlugin {
 
   static IMA_SDK_LIB_URL: string = "//imasdk.googleapis.com/js/sdkloader/ima3_debug.js";
 
-  imaLib;
-  imaPromise;
-
-  adContainerDiv: HTMLDivElement;
-  adDisplayContainer: any;
-  adsLoader: any;
+  imaLib: any;
+  imaPromise: Promise<*>;
+  mediaLoaded: boolean;
+  canPlayMedia: boolean;
+  playerLoaded: boolean;
+  initComplete: boolean;
+  adsActive: boolean;
   adsManager: any;
-  mediaLoaded: boolean = false;
-  target: HTMLDivElement;
-  playerLoaded: boolean = false;
-  playOnceReady: boolean = false;
-  adsActive: boolean = false;
-  canPlayMedia: boolean = false;
-  initComplete: boolean = false;
-  allAdsCompleted: boolean;
-  contentPlayheadTracker: Object = {
-    currentTime: 0,
-    previousTime: 0,
-    seeking: false,
-    duration: 0
-  };
+  _adContainerDiv: HTMLDivElement;
+  _adDisplayContainer: any;
+  _adsLoader: any;
+  _playOnceReady: boolean;
+  _allAdsCompleted: boolean;
+  _contentPlayheadTracker: Object;
 
   static isValid() {
     return true;
@@ -42,125 +36,39 @@ class ImaPlugin extends BasePlugin {
 
   constructor(name: string, player: Player, config: Object) {
     super(name, player, config);
-    this.target = this.player.getVideoElement();
-    this.imaPromise = script(ImaPlugin.IMA_SDK_LIB_URL);
+    this.imaPromise = loadScript(ImaPlugin.IMA_SDK_LIB_URL);
+    this.mediaLoaded = false;
+    this.canPlayMedia = false;
+    this.playerLoaded = false;
+    this.initComplete = false;
+    this.adsActive = false;
+    this._playOnceReady = false;
+    this._contentPlayheadTracker = {
+      currentTime: 0,
+      previousTime: 0,
+      seeking: false,
+      duration: 0
+    };
     this._addBindings();
   }
 
-  getPlayerDecorator(): PlayerDecoratorBase {
-    let plugin = this;
-
-    class ImaDecorator extends PlayerDecoratorBase {
-      load() {
-        return plugin.imaPromise.then(() => {
-          plugin.imaLib = window.google.ima;
-          plugin._initIMA();
-          if (!plugin._requestAds()) {
-            return super.load().then(() => {
-              plugin.mediaLoaded = true;
-              plugin.canPlayMedia = true;
-            });
-          }
-          plugin.playerLoaded = true;
-        });
-      }
-
-      play() {
-        if (!plugin.playerLoaded) {
-          this.load();
-          // If we don't have any ads - play the content
-          if (plugin.canPlayMedia) {
-            super.play();
-          }
-        }
-        if (!plugin.initComplete) {
-          plugin._initialUserAction();
-        } else {
-          if (plugin.adsActive) {
-            plugin.adsManager.resume();
-          } else {
-            super.play();
-          }
-        }
-      }
-
-      skipAd() {
-        if (plugin.adsManager && plugin.adsManager.getAdSkippableState()) {
-          plugin.adsManager.skip();
-        }
-      }
-
-      playAdNow(adTagURL: String) {
-        this.pause();
-        plugin.playAdNow(adTagURL);
-      }
-
-      pause() {
-        if (plugin.adsActive) {
-          plugin.adsManager.pause();
-        } else {
-          super.pause();
-        }
-      }
-    }
-
-    return new ImaDecorator();
-  }
-
-  _initialUserAction(): void {
-    this.adDisplayContainer.initialize();
-    if (this.adsManager) {
-      this.adsManager.start();
-      if (!this.mediaLoaded) {
-        this.player.load().then(() => {
-          this.mediaLoaded = true;
-        });
-      }
-    } else {
-      this.playOnceReady = true;
-    }
-    this.initComplete = true;
+  _addBindings(): void {
+    this.eventManager.listen(this.player, this.player.Event.TIME_UPDATE, this._updateCurrentTime.bind(this));
+    this.eventManager.listen(this.player, this.player.Event.ENDED, this._mediaEnded.bind(this));
   }
 
   _updateCurrentTime(): void {
-    if (!this.contentPlayheadTracker.seeking) {
-      this.contentPlayheadTracker.currentTime = this.player.currentTime;
+    if (!this._contentPlayheadTracker.seeking) {
+      this._contentPlayheadTracker.currentTime = this.player.currentTime;
     }
   }
 
-  _initIMA(): void {
-    if (!document.getElementById("adContainer")) {
-      this.adContainerDiv = this.target.parentNode.appendChild(document.createElement('div'));
-      this.adContainerDiv.id = "adContainer";
-      this.adContainerDiv.style.position = "absolute";
-      this.adContainerDiv.style.zIndex = 2000;
-      this.adContainerDiv.style.top = 0;
-      this.adDisplayContainer = new this.imaLib.AdDisplayContainer(this.adContainerDiv, this.player.getVideoElement());
-      this.adsLoader = new this.imaLib.AdsLoader(this.adDisplayContainer);
-      this.adsManager = null;
-      this.adsLoader.addEventListener(this.imaLib.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED, this._onAdsManagerLoaded, false, this);
-      this.adsLoader.addEventListener(this.imaLib.AdErrorEvent.Type.AD_ERROR, this._onAdError, false, this);
-    }
+  _showAdsContainer(): void {
+    this._adContainerDiv.style.display = "";
   }
 
-  playAdNow(tag: String): void {
-    this._resetIma();
-    this.config.adTagURL = tag;
-    if (!this.playerLoaded) {
-      this._initIMA();
-      this.playerLoaded = true;
-    }
-    this._requestAds();
-    this._initialUserAction();
-    this.adsManager.start();
-  }
-
-  showAdsContainer(): void {
-    this.adContainerDiv.style.display = "";
-  }
-
-  hideAdsContainer(): void {
-    this.adContainerDiv.style.display = "none";
+  _hideAdsContainer(): void {
+    this._adContainerDiv.style.display = "none";
   }
 
   _onAdError(params): void {
@@ -168,20 +76,19 @@ class ImaPlugin extends BasePlugin {
   }
 
   _onAdsManagerLoaded(adsManagerLoadedEvent: any): void {
-    this.logger.info('Ads loaded.');
+    this.logger.debug('Ads loaded');
     let adsRenderingSettings = new this.imaLib.AdsRenderingSettings();
     adsRenderingSettings.restoreCustomPlaybackStateOnAdBreakComplete = true;
-    this.adsManager = adsManagerLoadedEvent.getAdsManager(this.contentPlayheadTracker, adsRenderingSettings);
+    this.adsManager = adsManagerLoadedEvent.getAdsManager(this._contentPlayheadTracker, adsRenderingSettings);
     this._processAdsManager(this.adsManager);
   }
 
   _processAdsManager(adsManager: any): void {
-    debugger;
     // Attach the pause/resume events
     adsManager.addEventListener(this.imaLib.AdEvent.Type.CONTENT_PAUSE_REQUESTED, this._onContentPauseRequested, false, this);
     adsManager.addEventListener(this.imaLib.AdEvent.Type.CONTENT_RESUME_REQUESTED, this._onContentResumeRequested, false, this);
     // Handle errors
-    adsManager.addEventListener(this.imaLib.AdErrorEvent.Type.AD_ERROR, this.onAdError, false, this);
+    adsManager.addEventListener(this.imaLib.AdErrorEvent.Type.AD_ERROR, this._onAdError, false, this);
     let events = [
       this.imaLib.AdEvent.Type.ALL_ADS_COMPLETED,
       this.imaLib.AdEvent.Type.CLICK,
@@ -198,54 +105,117 @@ class ImaPlugin extends BasePlugin {
       adsManager.addEventListener(index, this._onAdEvent, false, this);
     }
 
-    let initWidth = parseInt(getComputedStyle(this.target).width, 10);
-    let initHeight = parseInt(getComputedStyle(this.target).height, 10);
+    let initWidth = parseInt(getComputedStyle(this.player.getVideoElement()).width, 10);
+    let initHeight = parseInt(getComputedStyle(this.player.getVideoElement()).height, 10);
 
     // TODO: Handle full screen
 
     adsManager.init(initWidth, initHeight, this.imaLib.ViewMode.NORMAL);
 
-    if (this.playOnceReady) {
+    if (this._playOnceReady) {
       adsManager.start();
     }
   }
 
-  //Ads events:
-  onAdError(adEvent: any) {
-    this.logger.info("onAdError:" + adEvent);
-  }
-
   _onContentPauseRequested(adEvent: any) {
-    this.logger.info("_onContentPauseRequested:" + adEvent);
-    this.showAdsContainer();
+    this.logger.debug("onContentPauseRequested");
+    this._showAdsContainer();
     this.adsActive = true;
   }
 
   _onContentResumeRequested(adEvent: any) {
-    debugger;
-    this.logger.info("_onContentResumeRequested:" + adEvent);
+    this.logger.debug("onContentResumeRequested");
     if (!this.contentComplete) {
-      this.hideAdsContainer();
+      this._hideAdsContainer();
       this.player.play();
       this.adsActive = false;
     }
   }
 
-  allAdsComplete(adEvent: any) {
-    this.hideAdsContainer();
-    this.allAdsCompleted = true;
+  _allAdsComplete(adEvent: any) {
+    this._hideAdsContainer();
+    this._allAdsCompleted = true;
   }
 
   _onAdEvent(adEvent: any) {
-    this.logger.info("_onAdEvent:" + adEvent.type);
+    this.logger.debug("onAdEvent: " + adEvent.type.toUpperCase());
     switch (adEvent.type) {
       case window.google.ima.AdEvent.Type.ALL_ADS_COMPLETED:
-        this.allAdsComplete(adEvent);
+        this._allAdsComplete(adEvent);
         break;
     }
   }
 
-  _requestAds(): boolean {
+  _mediaEnded(): void {
+    if (this._adsLoader && !this.contentComplete) {
+      this._adsLoader.contentComplete();
+      this.contentComplete = true;
+    }
+  }
+
+  _resetIma(): void {
+    this.adsActive = false;
+    this._hideAdsContainer();
+    if (this.adsManager) {
+      this.adsManager.destroy();
+      this.adsManager = null;
+    }
+    if (this._adsLoader && !this.contentComplete) {
+      this._adsLoader.contentComplete();
+    }
+    this.contentComplete = false;
+    this._allAdsCompleted = false;
+    this._playOnceReady = false;
+    this.canPlayMedia = false;
+  }
+
+  initIma(): void {
+    if (!document.getElementById("adContainer")) {
+      this._adContainerDiv = this.player.getVideoElement().parentNode.appendChild(document.createElement('div'));
+      this._adContainerDiv.id = "adContainer";
+      this._adContainerDiv.style.position = "absolute";
+      this._adContainerDiv.style.zIndex = 2000;
+      this._adContainerDiv.style.top = 0;
+      this._adDisplayContainer = new this.imaLib.AdDisplayContainer(this._adContainerDiv, this.player.getVideoElement());
+      this._adsLoader = new this.imaLib.AdsLoader(this._adDisplayContainer);
+      this.adsManager = null;
+      this._adsLoader.addEventListener(this.imaLib.AdsManagerLoadedEvent.Type.ADS_MANAGER_LOADED, this._onAdsManagerLoaded, false, this);
+      this._adsLoader.addEventListener(this.imaLib.AdErrorEvent.Type.AD_ERROR, this._onAdError, false, this);
+    }
+  }
+
+  playAdNow(tag: String): void {
+    this._resetIma();
+    this.config.adTagURL = tag;
+    if (!this.playerLoaded) {
+      this.initIma();
+      this.playerLoaded = true;
+    }
+    this.requestAds();
+    this.initialUserAction();
+    this.adsManager.start();
+  }
+
+  getPlayerDecorator(): PlayerDecoratorBase {
+    return new ImaDecorator(this);
+  }
+
+  initialUserAction(): void {
+    this._adDisplayContainer.initialize();
+    if (this.adsManager) {
+      this.adsManager.start();
+      if (!this.mediaLoaded) {
+        this.player.load().then(() => {
+          this.mediaLoaded = true;
+        });
+      }
+    } else {
+      this._playOnceReady = true;
+    }
+    this.initComplete = true;
+  }
+
+  requestAds(): boolean {
     this.logger.debug("requestAds");
     this._resetIma();
     let adsRequest = new this.imaLib.AdsRequest();
@@ -259,40 +229,12 @@ class ImaPlugin extends BasePlugin {
       return false;
     }
     //TODO: Handle non-linear
-    this.adsLoader.requestAds(adsRequest);
+    this._adsLoader.requestAds(adsRequest);
     return true;
   }
 
-  _addBindings(): void {
-    this.eventManager.listen(this.player, this.player.Event.TIME_UPDATE, this._updateCurrentTime.bind(this));
-    this.eventManager.listen(this.player, this.player.Event.ENDED, this._mediaEnded.bind(this));
-  }
-
-  _mediaEnded(): void {
-    if (this.adsLoader && !this.contentComplete) {
-      this.adsLoader.contentComplete();
-      this.contentComplete = true;
-    }
-  }
-
-  _resetIma(): void {
-    this.adsActive = false;
-    this.hideAdsContainer();
-    if (this.adsManager) {
-      this.adsManager.destroy();
-      this.adsManager = null;
-    }
-    if (this.adsLoader && !this.contentComplete) {
-      this.adsLoader.contentComplete();
-    }
-    this.contentComplete = false;
-    this.allAdsCompleted = false;
-    this.playOnceReady = false;
-    this.canPlayMedia = false;
-  }
-
   destroy(): void {
-    this.logger.info("in destroy");
+    this.logger.debug("destroy");
     this.eventManager.removeAll();
     this._resetIma();
   }
@@ -300,7 +242,7 @@ class ImaPlugin extends BasePlugin {
 
 registerPlugin(pluginName, ImaPlugin);
 
-function script(url) {
+function loadScript(url) {
   if (Array.isArray(url)) {
     let self = this, prom = [];
     url.forEach(function (item) {
--- test/src/ima.spec.js
+++ test/src/ima.spec.js
@@ -1,24 +1,24 @@
 import {playkit} from 'playkit-js'
-import ima from '../../src/ima.js'
+import ima from '../../src/ima'
 
 describe('ImaPlugin', function () {
   it('should play mp4 stream with ads', (done) => {
     window.player = playkit({
       "sources": [{
         "mimetype": "video/mp4",
-        "url": "http://www.html5videoplayer.net/videos/toystory.mp4",
+        "url": "http://www.html5videoplayer.net/videos/toystory.mp4"
       }],
       "plugins": {
         "ima": {
-          "adTagURL": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
+          // Pre-Mid-Post
+          "adTagURL": "http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpost&cmsid=496&vid=short_onecue&correlator="
+          // Pre
+          // "adTagURL": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
         }
       }
     });
-    player.load().then(() => {
-      player.play();
-      setTimeout(() => {
-        player.pause();
-      }, 5000);
+    window.player.load().then(() => {
+      window.player.play();
     });
   });
 });
