30e9d163c2fb8a63c35f8fb9cdc8d08c76040cbd
Dan Ziv <dan.ziv@kaltura.com>
2017-09-21 14:48:00 +0300

    add preload test

--- test/src/ima.spec.js
+++ test/src/ima.spec.js
@@ -329,4 +329,25 @@ describe('Ima Plugin', function () {
     });
     player.play();
   });
+
+  it('should play ads and then content with preload="auto"', (done) => {
+    let adPlayed = false;
+    player = loadPlayerWithAds(targetId, {
+      adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator='
+    }, {
+      preload: "auto"
+    });
+    ima = player._pluginManager.get('ima');
+    player.addEventListener(player.Event.AD_STARTED, () => {
+      adPlayed = true;
+    });
+    player.addEventListener(player.Event.PLAYING, () => {
+      if (adPlayed) {
+        done();
+      } else {
+        done(new Error("Content start without ads"));
+      }
+    });
+    player.play();
+  });
 });
