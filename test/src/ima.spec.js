c651dcca45f1312014d2cb580f5a3f3423b0c830
Oren Me <oren.me@gmail.com>
2017-11-19 21:45:41 +0200

    remove erronous test

--- test/src/ima.spec.js
+++ test/src/ima.spec.js
@@ -224,7 +224,7 @@ describe('Ima Plugin', function () {
     player.play();
   });
 
-  it.skip('should play single-redirect-linear', (done) => {
+  it('should play single-redirect-linear', (done) => {
     cuePoints = [0];
     player = loadPlayerWithAds(targetId, {
       adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirectlinear&correlator=[timestamp]'
@@ -239,7 +239,7 @@ describe('Ima Plugin', function () {
     player.play();
   });
 
-  it('should throw error because single-redirect-error', (done) => {
+  it.skip('should throw error because single-redirect-error', (done) => {
     player = loadPlayerWithAds(targetId, {
       adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirecterror&nofb=1&correlator=[timestamp]'
     });
