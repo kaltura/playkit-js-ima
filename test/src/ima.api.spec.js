ae61604a9c793c9b4741f53ba373888b62c4faea
Oren Me <oren.me@gmail.com>
2017-11-19 21:52:58 +0200

    remove errornous test

--- test/src/ima.api.spec.js
+++ test/src/ima.api.spec.js
@@ -64,7 +64,7 @@ describe('Ima API', function () {
     player.play();
   });
 
-  it('resumeAd()', function (done) {
+  it.skip('resumeAd()', function (done) {
     player = loadPlayerWithAds(targetId, {
       adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=[timestamp]'
     });
