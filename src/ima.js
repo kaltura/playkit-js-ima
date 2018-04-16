ec3124493984ffbb802011881308025d4907f046
Yair Ansbacher <yair.ansbacher@kaltura.com>
2018-04-16 16:51:42 +0300

    fix(FEC-8013): Play/Pause actions are not working when tapping the up… (#55)
    hide the ads container by default

--- src/ima.js
+++ src/ima.js
@@ -479,8 +479,6 @@ export default class Ima extends BasePlugin {
     this._adsCoverDiv.onclick = () => this._onAdsCoverClicked();
     // Append the ads container to the dom
     Utils.Dom.appendChild(playerView, this._adsContainerDiv);
-    // Hides the ads container after appending it to the DOM
-    this._hideAdsContainer();
     this._adDisplayContainer = new this._sdk.AdDisplayContainer(this._adsContainerDiv, this.player.getVideoElement());
   }
 
@@ -682,7 +680,7 @@ export default class Ima extends BasePlugin {
    */
   _showAdsContainer(): void {
     if (this._adsContainerDiv) {
-      this._adsContainerDiv.style.display = "";
+      this._adsContainerDiv.style.visibility = "visible";
     }
   }
 
@@ -693,7 +691,7 @@ export default class Ima extends BasePlugin {
    */
   _hideAdsContainer(): void {
     if (this._adsContainerDiv) {
-      this._adsContainerDiv.style.display = "none";
+      this._adsContainerDiv.style.visibility = "hidden";
     }
   }
 
