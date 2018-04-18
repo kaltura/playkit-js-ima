2700072598d68d7ba5582734e293e7bdbc82ee49
Dan Ziv <dan.ziv@kaltura.com>
2018-04-18 10:11:16 +0300

    FEC-8129

--- src/ima.js
+++ src/ima.js
@@ -347,8 +347,9 @@ export default class Ima extends BasePlugin {
       this._nextPromise = Utils.Object.defer();
       this._adDisplayContainer.initialize();
       this._hasUserAction = true;
-      if (!this.config.adTagUrl) {
+      if (!this.config.adTagUrl && !this.config.adsResponse) {
         this._resolveNextPromise();
+        return this._nextPromise;
       }
       if (this._isAdsManagerLoaded) {
         this.logger.debug("User action occurred after ads manager loaded");
