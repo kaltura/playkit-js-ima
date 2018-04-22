7cce0d5b87b068ea39d37da4a56968eb9251c98e
Oren Me <oren.me@gmail.com>
2018-04-22 15:04:40 +0300

    fix travis runtime flag

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ const isMacOS = /^darwin/.test(process.platform);
 const customLaunchers = {
   Chrome_travis_ci: {
     base: 'Chrome',
-    flags: ['--no-sandbox', '--disable-autoplay-policy']
+    flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required']
   }
 };
 
