64aeb21b52dca45bc9c9313ead9f7cb190b5a424
Dan Ziv <dan.ziv@kaltura.com>
2018-04-22 23:09:44 +0300

    fix(FEC-8131): skippable ads fail on iOS (#58)

--- karma.conf.js
+++ karma.conf.js
@@ -10,15 +10,15 @@ const isMacOS = /^darwin/.test(process.platform);
 const customLaunchers = {
   Chrome_travis_ci: {
     base: 'Chrome',
-    flags: ['--no-sandbox']
+    flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required']
   }
 };
 
 module.exports = function (config) {
   let karmaConf = {
     logLevel: config.LOG_INFO,
-    browserDisconnectTimeout:30000,    
-    browserNoActivityTimeout:60000,
+    browserDisconnectTimeout: 30000,
+    browserNoActivityTimeout: 60000,
     browsers: [
       'Chrome',
       'Firefox'
