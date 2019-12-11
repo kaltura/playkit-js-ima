3c092852d8c9e83509946a59a65dd40399928425
Yuval keidar <yuval.keidar@kaltura.com>
2019-12-11 16:20:19 +0200

    Update after_deploy.sh

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -d "{'name':$1, 'version':$2}" -H "Content-Type: application/json" -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$3
+curl -k -d "{'name':$1, 'version':$2}" -H "Content-Type: application/json" -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$3
