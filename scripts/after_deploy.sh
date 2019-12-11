1bd0bd1ca72a4acc0b08244f7660d2abfc675f50
Yuval keidar <yuval.keidar@kaltura.com>
2019-12-11 16:13:16 +0200

    fix domain

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,1 +1,2 @@
-curl -d "{'name':$1, 'version':$2}" -H "Content-Type: application/json" -X POST https://travisci.kaltura.com/generic-webhook-trigger/invoke?token=$3
+#!/bin/bash
+curl -d "{'name':$1, 'version':$2}" -H "Content-Type: application/json" -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$3
