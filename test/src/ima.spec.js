561d7cc60a9890846da1ae22f96a7ad5560e733b
Yair Ansbacher <yair.ansbacher@kaltura.com>
2019-12-12 10:36:41 +0200

    chore: ad layout tests (#154)
    add unit tests for `playAdNow` api

--- test/src/ima.spec.js
+++ test/src/ima.spec.js
@@ -548,8 +548,12 @@ describe('Ima Plugin', function() {
     );
     ima = player._pluginManager.get('ima');
     player.addEventListener(player.Event.AD_MANIFEST_LOADED, () => {
-      ima._adsManager.h.playAdsAfterTime.should.equal(15);
-      done();
+      try {
+        ima._adsManager.w && typeof ima._adsManager.w.playAdsAfterTime === 'number' && ima._adsManager.w.playAdsAfterTime.should.equal(15);
+        done();
+      } catch (e) {
+        done(e);
+      }
     });
     player.play();
   });
@@ -570,9 +574,289 @@ describe('Ima Plugin', function() {
     );
     ima = player._pluginManager.get('ima');
     player.addEventListener(player.Event.AD_MANIFEST_LOADED, () => {
-      ima._adsManager.h.playAdsAfterTime.should.equal(-1);
-      done();
+      try {
+        ima._adsManager.w && typeof ima._adsManager.w.playAdsAfterTime === 'number' && ima._adsManager.w.playAdsAfterTime.should.equal(-1);
+        done();
+      } catch (e) {
+        done(e);
+      }
     });
     player.play();
+  });
+
+  it('should return _playAdByConfig true for adTagUrl configured', function() {
+    player = loadPlayerWithAds(
+      targetId,
+      {
+        adTagUrl:
+          'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=[timestamp]'
+      },
+      {
+        startTime: 15
+      }
+    );
+    ima = player._pluginManager.get('ima');
+    ima._playAdByConfig().should.be.true;
+  });
+
+  it('should return _playAdByConfig true for adsResponse configured', function() {
+    player = loadPlayerWithAds(
+      targetId,
+      {
+        adsResponse:
+          'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=[timestamp]'
+      },
+      {
+        startTime: 15
+      }
+    );
+    ima = player._pluginManager.get('ima');
+    ima._playAdByConfig().should.be.true;
+  });
+
+  it('should return _playAdByConfig false for neither adTagUrl or adsResponse configured', function() {
+    player = loadPlayerWithAds(
+      targetId,
+      {},
+      {
+        startTime: 15
+      }
+    );
+    ima = player._pluginManager.get('ima');
+    ima._playAdByConfig().should.be.false;
+  });
+
+  describe('playAdNow', function() {
+    let sandbox;
+    const vasts = [
+      'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=[timestamp]',
+      'https://pubads.g.doubleclick.net/gampad/ads?slotname=%2F124319096%2Fexternal%2Fad_rule_samples&sz=640x480&ciu_szs=728x90%2C300x250%2C180x150%2C120x60%2C88x31%2C300x60%2C300x100%2C320x50%2C468x60%2C300x600%2C160x600&cust_params=deployment%3Ddevsite%26sample_ar%3Dpreonlybumper&url=https%3A%2F%2Fdevelopers.google.com%2Finteractive-media-ads%2Fdocs%2Fsdks%2Fhtml5%2Fvastinspector&unviewed_position_start=1&output=xml_vast4&env=vp&gdfp_req=1&ad_rule=0&vad_type=linear&vpos=preroll&pod=1&bumper=after&min_ad_duration=0&max_ad_duration=10000&vrid=5896&sb=1&is_amp=0&dt=1568197624492&idt=1045&dlt=1568197616840&adk=4018648594&correlator=4086141714122770&scor=2715330385634426&ged=ve4_td7_tt6_pd7_la0_er316.20.676.660_vi0.0.2540.509_vp76_ts0_eb20331&osd=2&cdm=google-developers.appspot.com&cookie_enabled=1&vis=1&jar=2019-9-11-8&hl=he&frm=2&video_doc_id=short_onecue&cmsid=496&sdkv=h.3.329.0&sdki=44d&mpt=h5_vsi&sdr=1&eid=651800008&u_so=l&afvsz=200x200%2C250x250%2C300x250%2C336x280%2C450x50%2C468x60%2C480x70&kfa=0&tfcd=0&adsid=ChAI8Lbi6wUQ8_Xq75y2n6gzEkwAtiXUNfqKuVR9qwyD1lwdPg36ANqqxEhxk1XLd5vpe1rNXc2uC400F_Kniq1_Dtsas5O7A5qH1IZWUFgWFWgnfwrgqdvt__nSPwdd'
+    ];
+
+    beforeEach(() => {
+      player = loadPlayerWithAds(targetId, {}, {autoplay: true});
+      ima = player._pluginManager.get('ima');
+      sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it('should play given single preroll', function(done) {
+      player.addEventListener(player.Event.AD_BREAK_START, event => {
+        try {
+          event.payload.adBreak.position.should.equal(0);
+          event.payload.adBreak.type.should.equal(AdBreakType.PRE);
+          event.payload.adBreak.numAds.should.equal(1);
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+      ima.playAdNow([{url: [vasts[0]]}]);
+    });
+
+    it('should play given single midroll', function(done) {
+      player.addEventListener(player.Event.AD_BREAK_START, event => {
+        try {
+          event.payload.adBreak.position.should.gt(1);
+          event.payload.adBreak.type.should.equal(AdBreakType.MID);
+          event.payload.adBreak.numAds.should.equal(1);
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+      setTimeout(() => {
+        ima.playAdNow([{url: [vasts[0]]}]);
+      }, 1500);
+    });
+
+    it('should play given single postroll', function(done) {
+      player.addEventListener(player.Event.AD_BREAK_START, event => {
+        try {
+          event.payload.adBreak.position.should.equal(-1);
+          event.payload.adBreak.type.should.equal(AdBreakType.POST);
+          event.payload.adBreak.numAds.should.equal(1);
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+      player.addEventListener(player.Event.ENDED, () => {
+        ima.playAdNow([{url: [vasts[0]]}]);
+      });
+      player.addEventListener(player.Event.FIRST_PLAYING, () => {
+        player.currentTime = player.duration;
+      });
+    });
+
+    it('should not play while ad is playing', function(done) {
+      player.addEventListener(player.Event.AD_BREAK_START, () => {
+        player.addEventListener(player.Event.AD_ERROR, () => {
+          done(new Error('should not play while ad playing'));
+        });
+        player.addEventListener(player.Event.AD_FIRST_QUARTILE, () => {
+          done();
+        });
+        ima.playAdNow([{url: [vasts[0]]}]);
+      });
+      ima.playAdNow([{url: [vasts[0]]}]);
+    });
+
+    it('should not play when adTagUrl or adsResponse configured', function(done) {
+      sandbox.stub(ima, '_playAdByConfig').callsFake(function() {
+        return true;
+      });
+      player.addEventListener(player.Event.AD_BREAK_START, () => {
+        done(new Error('should not play while ad playing'));
+      });
+
+      ima.playAdNow([{url: [vasts[0]]}]);
+      setTimeout(done, 2000);
+    });
+
+    it('should play ad pod - next once first complete', function(done) {
+      player.addEventListener(player.Event.AD_COMPLETED, () => {
+        player.addEventListener(player.Event.AD_STARTED, () => {
+          done();
+        });
+      });
+      ima.playAdNow([{url: [vasts[0]]}, {url: [vasts[1]]}]);
+    });
+
+    it('should play ad pod - next once first skipped', function(done) {
+      player.addEventListener(player.Event.AD_CAN_SKIP, () => {
+        player.addEventListener(player.Event.AD_SKIPPED, () => {
+          player.addEventListener(player.Event.AD_STARTED, () => {
+            done();
+          });
+        });
+        ima.skipAd();
+      });
+      ima.playAdNow([{url: [vasts[0]]}, {url: [vasts[1]]}]);
+    });
+
+    it('should play ad pod - next once first failed', function(done) {
+      player.addEventListener(player.Event.AD_ERROR, () => {
+        player.addEventListener(player.Event.AD_STARTED, () => {
+          done();
+        });
+      });
+      ima.playAdNow([{url: ['bad/url']}, {url: [vasts[1]]}]);
+    });
+
+    it('should fire AD_BREAK_START, AD_BREAK_END and ADS_COMPLETED once for ad pod', function(done) {
+      player.addEventListener(player.Event.AD_BREAK_START, event => {
+        try {
+          event.payload.adBreak.position.should.equal(0);
+          event.payload.adBreak.type.should.equal(AdBreakType.PRE);
+          event.payload.adBreak.numAds.should.equal(2);
+        } catch (e) {
+          done(e);
+        }
+        player.addEventListener(player.Event.AD_BREAK_START, () => {
+          done(new Error('AD_BREAK_START should be triggered once for ad pod'));
+        });
+        player.addEventListener(player.Event.AD_CAN_SKIP, () => {
+          player.addEventListener(player.Event.AD_BREAK_END, () => {
+            player.addEventListener(player.Event.AD_BREAK_END, () => {
+              done(new Error('AD_BREAK_END should be triggered once for ad pod'));
+            });
+            player.addEventListener(player.Event.ADS_COMPLETED, () => {
+              player.addEventListener(player.Event.ADS_COMPLETED, () => {
+                done(new Error('ALL_ADS_COMPLETED should be triggered once for ad pod'));
+              });
+              done();
+            });
+          });
+          ima.skipAd();
+        });
+      });
+      ima.playAdNow([{url: [vasts[0]]}, {url: [vasts[1]]}]);
+    });
+
+    it('should fire AD_BREAK_END and ADS_COMPLETED once the second ad failed', function(done) {
+      player.addEventListener(player.Event.AD_ERROR, () => {
+        player.addEventListener(player.Event.AD_BREAK_END, () => {
+          player.addEventListener(player.Event.ADS_COMPLETED, () => {
+            done();
+          });
+        });
+      });
+      ima.playAdNow([{url: [vasts[1]]}, {url: ['bad/url']}]);
+    });
+
+    it('should fire only ADS_COMPLETED once the whole ad pod failed', function(done) {
+      player.addEventListener(player.Event.AD_BREAK_END, () => {
+        done(new Error('AD_BREAK_END should not be triggered when the whole ad pod failed'));
+      });
+      player.addEventListener(player.Event.ADS_COMPLETED, () => {
+        done();
+      });
+      ima.playAdNow([{url: ['bad/url']}, {url: ['bad/url']}]);
+    });
+
+    it('should fire AD_LOADED with correct params', function(done) {
+      const onFirstLoaded = event => {
+        player.removeEventListener(player.Event.AD_LOADED, onFirstLoaded);
+        try {
+          event.payload.ad.position.should.equal(1);
+          event.payload.ad.bumper.should.be.false;
+        } catch (e) {
+          done(e);
+        }
+        player.addEventListener(player.Event.AD_CAN_SKIP, () => {
+          player.addEventListener(player.Event.AD_LOADED, event => {
+            try {
+              event.payload.ad.position.should.equal(2);
+              event.payload.ad.bumper.should.be.true;
+              done();
+            } catch (e) {
+              done(e);
+            }
+          });
+          ima.skipAd();
+        });
+      };
+      player.addEventListener(player.Event.AD_LOADED, onFirstLoaded);
+      ima.playAdNow([{url: [vasts[0]]}, {bumper: true, url: [vasts[1]]}]);
+    });
+
+    it('should fire AD_WATERFALLING instead of AD_ERROR once the first url failed and waterfall and play next', function(done) {
+      player.addEventListener(player.Event.AD_ERROR, () => {
+        done(new Error('AD_ERROR should not be triggered when waterfall exists'));
+      });
+      player.addEventListener(player.Event.AD_WATERFALLING, event => {
+        try {
+          event.payload.adFailed.url.should.equal('bad/url');
+          event.payload.adFailed.adBreak.should.equal(AdBreakType.PRE);
+          event.payload.adFailed.position.should.equal(1);
+        } catch (e) {
+          done(e);
+        }
+        player.addEventListener(player.Event.AD_CAN_SKIP, () => {
+          player.addEventListener(player.Event.AD_LOADED, () => {
+            done();
+          });
+          ima.skipAd();
+        });
+      });
+      ima.playAdNow([{url: ['bad/url', vasts[0]]}, {url: [vasts[1]]}]);
+    });
+
+    it('should fire AD_WATERFALLING_FAILED and AD_ERROR once the all urls failed and play next', function(done) {
+      player.addEventListener(player.Event.AD_WATERFALLING, () => {
+        player.addEventListener(player.Event.AD_WATERFALLING_FAILED, () => {
+          player.addEventListener(player.Event.AD_ERROR, () => {
+            player.addEventListener(player.Event.AD_LOADED, () => {
+              done();
+            });
+          });
+        });
+      });
+      ima.playAdNow([{url: ['bad/url', 'bad/url']}, {url: [vasts[1]]}]);
+    });
   });
 });
