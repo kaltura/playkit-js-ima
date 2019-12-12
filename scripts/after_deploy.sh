97c10b9917ee916590b66116c9695ba473b845c7
Yuval keidar <yuval.keidar@kaltura.com>
2019-12-12 17:43:33 +0200

    test curl

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -k -d "{'name':$1, 'version':$2}" -H "Content-Type: application/json" -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$3
+curl -k -d "{'name':$1, 'version':$2 'source':$3}" -H "Content-Type: application/json" -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$4
