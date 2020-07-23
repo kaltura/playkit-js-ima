fb40564762df85af09067404d15d6d5a0e4b851c
yuval.keidar <yuval.keidar@kaltura.com>
2020-07-23 17:23:31 +0300

    chore(FEC-10335): add tests for playAdsWithMSE config

--- test/src/ima.spec.js
+++ test/src/ima.spec.js
@@ -31,6 +31,65 @@ describe('Ima Plugin', function() {
     TestUtils.removeElement(targetId);
   });
 
+  it('playback should keeps playing after midroll finished with playAdsWithMSE', done => {
+    player = loadPlayerWithAds(
+      targetId,
+      {
+        adTagUrl:
+          'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpost&cmsid=496&vid=short_onecue&correlator='
+      },
+      {playback: {playAdsWithMSE: true}}
+    );
+    ima = player._pluginManager.get('ima');
+    const keepsPlayingAfterAd = () => {
+      player.removeEventListener(player.Event.AD_BREAK_END, keepsPlayingAfterAd);
+      player.currentTime = 15;
+      player.addEventListener(player.Event.AD_BREAK_END, () => {
+        player.addEventListener(player.Event.PLAYING, () => done());
+      });
+    };
+    player.addEventListener(player.Event.AD_BREAK_END, keepsPlayingAfterAd);
+    player.play();
+  });
+
+  it('playback should keeps playing after postroll finished with playAdsWithMSE', done => {
+    player = loadPlayerWithAds(
+      targetId,
+      {
+        adTagUrl:
+          'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpostonly&cmsid=496&vid=short_onecue&correlator='
+      },
+      {playback: {playAdsWithMSE: true}}
+    );
+    ima = player._pluginManager.get('ima');
+    const playing = () => {
+      player.removeEventListener(player.Event.PLAYING, playing);
+      player.currentTime = player.duration;
+      player.addEventListener(player.Event.AD_BREAK_END, () => {
+        player.addEventListener(player.Event.PLAYING, () => done());
+        player.play();
+      });
+    };
+    player.addEventListener(player.Event.PLAYING, playing);
+    player.play();
+  });
+
+  it('playback should not be affected with not linear ad and playAdsWithMSE', done => {
+    player = loadPlayerWithAds(
+      targetId,
+      {
+        adTagUrl:
+          'https://pubads.g.doubleclick.net/gampad/ads?sz=480x70&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dnonlinear&correlator='
+      },
+      {playback: {playAdsWithMSE: true}}
+    );
+    ima = player._pluginManager.get('ima');
+    player.addEventListener(player.Event.PLAYING, () => {
+      setTimeout(done, 5000);
+    });
+    player.play();
+  });
+
   it('should fire all ads events', done => {
     player = loadPlayerWithAds(targetId, {
       adTagUrl:
