92c025b35502b670b52c4f018db8307c5a717343
Roy Bregman <roy.bregman@kaltura.com>
2020-11-30 20:32:34 +0200

    fix(FEC-10730): [IMA][Zee5] High number of postrolls

--- src/ima.js
+++ src/ima.js
@@ -872,6 +872,9 @@ class Ima extends BasePlugin implements IMiddlewareProvider, IAdsControllerProvi
       adsRequest.linearAdSlotHeight = this.player.dimensions.height;
       adsRequest.nonLinearAdSlotWidth = this.player.dimensions.width;
       adsRequest.nonLinearAdSlotHeight = this.player.dimensions.height / 3;
+      if (typeof this.player.config.sources.duration === 'number') {
+        adsRequest.contentDuration = this.getContentDuration();
+      }
 
       const muted = this.player.muted || this.player.volume === 0;
       adsRequest.setAdWillPlayMuted(muted);
