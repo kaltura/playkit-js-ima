808f3c0340041d3d3995c4f8ed4145413a051eaa
JonathanTGold <jonathan.gold@86@gmail.com>
2022-01-11 16:55:06 +0200

    fix

--- flow-typed/types/omid-access-modes-config.js
+++ flow-typed/types/omid-access-modes-config.js
@@ -1,7 +1,20 @@
 // @flow
 
+/**
+*  {@link https://developers.google.com/interactive-media-ads/docs/sdks/html5/client-side/omsdk#access_modes different access modes for verification scripts}
+ * @typedef {( 'FULL' | 'CREATIVE' | 'DOMAIN' | 'LIMITED')} AccessMode
+ */
 declare type AccessMode = 'FULL' | 'CREATIVE' | 'DOMAIN' | 'LIMITED';
 
+/**
+ * @typedef OmidAccessModesConfig
+ * @example
+ * {
+ *   MOAT: 'FULL'
+ *   PIXELATE: 'CREATIVE'
+ *   OTHER: 'DOMAIN'
+ * }
+ */
 declare type OmidAccessModesConfig = {
   OTHER?: AccessMode,
   MOAT?: AccessMode,
