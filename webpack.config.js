0642582629bed7e2312e04313f9902264692e350
Roee Dean <132472362+roeedean@users.noreply.github.com>
2026-01-25 12:32:39 +0200

    feat(FEC-14869): Adding nonce support in player plugins style tags (#267)
    Co-authored-by: roeedean <ovp-github-actions@kaltura.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const packageData = require('./package.json');
+const {insertStylesWithNonce} = require('@playkit-js/webpack-common');
 
 let plugins = [
   new webpack.DefinePlugin({
@@ -52,7 +53,13 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: 'style-loader',
+            options: {
+              attributes: {
+                id: `${packageData.name}`
+              },
+              insert: insertStylesWithNonce
+            }
           },
           {
             loader: 'css-loader'
